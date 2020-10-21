8a7bf3c678afa8129e32fdc06e70eb0649806775
Manlio E Teran Ramos <manlioelnum1@hotmail.com>
2020-10-21 00:19:48 -0500

    chat

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const http = require('http');
-const port = process.env.PORT || 80
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
+let app = require('express')();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+
+    socket.on('disconnect', function() {
+        io.emit('users-changed', { user: socket.username, event: 'left' });
+    });
+
+    socket.on('set-name', (name) => {
+        socket.username = name;
+        io.emit('users-changed', { user: name, event: 'joined' });
+    });
+
+    socket.on('send-message', (message) => {
+        io.emit('message', { msg: message.text, user: socket.username, createdAt: new Date() });
+    });
 });
-server.listen(port, () => {
-    console.log(`Server running at port ` + port);
+
+var port = process.env.PORT || 80;
+
+server.listen(port, function() {
+    console.log('listening in https://chat-legal.azurewebsites.net:' + port);
 });
